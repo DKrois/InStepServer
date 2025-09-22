9bc81fdeabf9365197c07649e0263020d6fc788a
DKrois <231201892+DKrois@users.noreply.github.com>
2025-09-22 15:08:23 +0200

    basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+import express from 'express';
+import { port } from './config.json' with { type: 'json' };
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static('public'));
+app.get('/api', handleGETRequest);
+app.post('/api', handlePOSTRequest);
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+async function handleGETRequest(req: express.Request, res: express.Response) {
+    res.send('heyyy');
+}
+
+async function handlePOSTRequest(req: express.Request, res: express.Response) {
+    res.send('heyyy2');
+}
+
+// GUI for server ?
+// repo for IDM?
