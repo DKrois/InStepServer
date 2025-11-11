07748b21d6df3a13ff07bdde8399a7674935193d
DKrois <231201892+DKrois@users.noreply.github.com>
2025-11-11 09:56:20 +0100

    Release v1.0.2 (update testing)

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -10,29 +10,26 @@ if (-not $Version.StartsWith('v')) {
     $Version = "v$Version"
 }
 
+# make sure repo is up-to-date
+git add .
+git commit -m "Release $Version"
+git push origin master
+
+Write-Host "-- Pushed to git"
+
 if ($ReRelease) {
-    # 1. Delete the tag from your remote repository on GitHub
+    # delete tags
     git push origin --delete $Version
-    # 2. Delete the tag from your local repository
     git tag -d $Version
+    Write-Host "-- Deleted tag from local and remote repository"
 
-    Write-Host "Deleted tag from local and remote repository"
-
-    # 3. (Optional but recommended) Make any code changes needed to fix the build
-    #    For example, remove package-lock.json from .gitignore
-    git add .
-    git commit -m "Release $Version"
-    git push origin master
-
-    Write-Host "Pushed to git"
-
-    # 4. Re-create the tag on your latest commit
+    # recreate tag
     git tag $Version
-    Write-Host "Created new tag"
+    Write-Host "-- Recreated tag"
 
-    # 5. Push the new tag to GitHub, which will trigger the workflow again
+    # push new tag to GitHub, which will trigger the workflow again
     git push origin $Version
-    Write-Host "Done!"
+    Write-Host "-- Done!"
 } else {
     git tag $Version
     git push origin $Version
